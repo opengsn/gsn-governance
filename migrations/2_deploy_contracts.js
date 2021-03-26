cfa37a406badccbf780e2807ef4f6474249311da
Dror Tirosh <dror@opengsn.org>
2021-03-26 21:13:20 +0300

    updated to ropsten deployment.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,7 +10,8 @@ const TreasuryVester = artifacts.require('TreasuryVester')
 module.exports = async function (deployer, network, accounts) {
   const account = accounts[0]
   const minter = accounts[0]
-  const trustedForwarder = '0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab'
+
+  const trustedForwarder = '0x7Fe7c7B71E9882651c14D5771ECAa06cC6b27882'
   const mintingAllowedAfter = Date.now() + 1
   await deployer.deploy(Uni, account, minter, mintingAllowedAfter, trustedForwarder)
   const transactionCount = await web3.eth.getTransactionCount(account)
@@ -19,19 +20,27 @@ module.exports = async function (deployer, network, accounts) {
     ethereumjsUtil.toBuffer(transactionCount + 1)))
   const delay = 172800
   await deployer.deploy(Timelock, futureGovernorAddress, delay)
-  await deployer.deploy(GovernorAlpha, Timelock.address, Uni.address)
+  await deployer.deploy(GovernorAlpha, Timelock.address, Uni.address, trustedForwarder)
 
   assert.equal(GovernorAlpha.address.toLowerCase(), futureGovernorAddress, 'calculated governor address does not match')
 
   const recipients = [
     {
-      address: '0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0',
-      vestingAmount: '100'
+      address: '0xd21934eD8eAf27a67f0A70042Af50A1D6d195E81',
+      vestingAmount: '10000'
+    },
+    {
+      address: '0x7149173Ed76363649675C3D0684cd4Bac5A1006d',
+      vestingAmount: '10000'
+    },
+    {
+      address: '0xF61591e478f83C14C22DF18A21AdcA24037005DD',
+      vestingAmount: '10000'
     }
   ]
   const vestingBegin = Date.now() + 1
-  const vestingCliff = Date.now() + 100
-  const vestingEnd = Date.now() + 1000
+  const vestingCliff = Date.now() + 200
+  const vestingEnd = Date.now() + 3600
   const canVote = true
 
   for (const recipient of recipients) {
--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require('truffle-hdwallet-provider')
+var mnemonic = 'digital unknown jealous mother legal hedgehog save glory december universe spread figure custom found six'
+
 module.exports = {
   compilers: {
     solc: {
@@ -12,6 +15,13 @@ module.exports = {
     }
   },
   networks: {
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/f40be2b1a3914db682491dc62a19ad43')
+      },
+      skipDryRun: true,
+      network_id: 4
+    },
     development: {
       // we run Ganache with chainID 1 to test the react app; no need for a dry run
       skipDryRun: true,
