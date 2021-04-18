a7209586e8ad62635360a9d7cb81e2572a29f47f
Dror Tirosh <dror@opengsn.org>
2021-04-18 20:06:59 +0300

    extracted Onwable abi

---
+++ deploy-scripts/OwnableAbi.js
@@ -0,0 +1,30 @@
+const ownableAbi = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+module.exports = {ownableAbi}
--- deploy-scripts/gsn-setowner.js
+++ deploy-scripts/gsn-setowner.js
@@ -1,58 +1,31 @@
 //this script should run in "gsn" folder..
-const Timelock = artifacts.require('Timelock')
+const {ownableAbi} = require("./OwnableAbi");
 
 // Transfer non-vested tokens
 module.exports = async function (callback) {
-  try {
-    for ( contract of [ 'RelayHub', 'VersionRegistry' ] ) {
-      let contractAddress = process.env[contract + 'Address'];
-      console.log(`${contract} at ${contractAddress}`)
-      const hub = new web3.eth.Contract([
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "newOwner",
-              "type": "address"
+    try {
+        for (contract of ['RelayHub', 'VersionRegistry']) {
+            let contractAddress = process.env[contract + 'Address'];
+            console.log(`${contract} at ${contractAddress}`)
+            const hub = new web3.eth.Contract(ownableAbi, contractAddress)
+
+            const accounts = await web3.eth.getAccounts()
+            const currentOwner = await hub.methods.owner().call()
+            const from = accounts[0]
+
+            const timelockAddress = process.env.TIMELOCK
+            if (currentOwner === timelockAddress) {
+                console.log(`${contract}: owner already set`)
+            } else if (currentOwner !== from) {
+                console.error(`FATAL: ${contract}: current owner is ${currentOwner}, but from is ${from}.\n\tCan't setOwner to timelock ${timelockAddress}`)
+                process.exit(1)
+            } else {
+                await hub.methods.transferOwnership(timelockAddress).send({from}).catch(e => console.log(e))
+                console.log(`Done setting ${contract} @ ${hub.options.address} owner to Timelock ${timelockAddress}`)
             }
-          ],
-          "name": "transferOwnership",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "owner",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
         }
-      ], contractAddress)
-
-      const accounts = await web3.eth.getAccounts()
-      const currentOwner = await hub.methods.owner().call()
-      const from = accounts[0]
-
-      timelockAddress = process.env.TIMELOCK
-      if (currentOwner === timelockAddress) {
-        console.log(`${contract}: owner already set`)
-      } else if (currentOwner !== from) {
-        console.error(`FATAL: ${contract}: current owner is ${currentOwner}, but from is ${from}.\n\tCan't setOwner to timelock ${timelockAddress}`)
-        process.exit(1)
-      } else {
-        await hub.methods.transferOwnership(timelockAddress).send({from}).catch(e => console.log(e))
-        console.log(`Done setting ${contract} @ ${hub.options.address} owner to Timelock ${timelockAddress}`)
-      }
+        callback()
+    } catch (e) {
+        callback(e)
     }
-    callback()
-  } catch(e) {
-    callback(e)
-  }
 }
