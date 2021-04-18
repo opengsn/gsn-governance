ab5e34bdb7947e4a33ea9d6acb780ab5409a8113
Dror Tirosh <dror@opengsn.org>
2021-04-18 21:16:40 +0300

    fix debug-no-git in debug contracts

--- deploy-scripts/3-deploy-gov.sh
+++ deploy-scripts/3-deploy-gov.sh
@@ -6,8 +6,8 @@ source $GSN_OUT
 
 if [ -z "$IS_MAINNET" ] ; then
   echo "== DEBUG: modify Timelock, GovernorAlpha to accept minute delays (instead of week)"
-  perl -pi -e  's!2 days!1 minutes /* DEBUG - [N]OGIT */ !' ../contracts/Timelock.sol
-  perl -pi -e  's!40_320!15 /* DEBUG - 3 minutes - [N]OGIT */ !' ../contracts/GovernorAlpha.sol
+  perl -pi -e  's!2 days!1 minutes /* DEBUG - NO\GIT */ !' ../contracts/Timelock.sol
+  perl -pi -e  's!40_320!15 /* DEBUG - 3 minutes - NO\GIT */ !' ../contracts/GovernorAlpha.sol
 
   grep -q DEBUG ../contracts/Timelock.sol || fatal failed to patch Timelock.
   grep -q DEBUG ../contracts/GovernorAlpha.sol || fatal failed to patch GovernorAlpha.
